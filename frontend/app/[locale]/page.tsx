aa63f70fa5309390830f931ccf46bcd454ae88e1
Iewnfod <shangzhiqiuyue@outlook.com>
2025-10-12 23:34:02 +0800

    remove(frontend): home page

--- frontend/app/[locale]/page.tsx
+++ frontend/app/[locale]/page.tsx
@@ -1,55 +1,32 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
+"use client";
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import {useEffect} from "react";
+import {useTransitionRouter} from "next-transition-router";
+import {get} from "@/utils/network";
+import {ApiResponse} from "../../../lib/types/api";
+import {User} from "../../../lib/types/user";
+import {addToast} from "@heroui/toast";
 
 export default function Home() {
+    const router = useTransitionRouter();
+
+    useEffect(() => {
+        get("/user/fetch").then((data: ApiResponse<User>) => {
+            if (data.success) {
+                if (data.data) {
+                    localStorage.setItem("user", JSON.stringify(data.data));
+                    router.push("/project");
+                } else {
+                    addToast({
+                        title: data.error,
+                        color: "danger"
+                    });
+                }
+            }
+        });
+    }, []);
+
     return (
-        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-            <div className="inline-block max-w-xl text-center justify-center">
-                <span className={title()}>Make&nbsp;</span>
-                <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-                <br />
-                <span className={title()}>
-                    websites regardless of your design experience.
-                </span>
-                <div className={subtitle({ class: "mt-4" })}>
-                    Beautiful, fast and modern React UI library.
-                </div>
-            </div>
-
-            <div className="flex gap-3">
-                <Link
-                    isExternal
-                    className={buttonStyles({
-                        color: "primary",
-                        radius: "full",
-                        variant: "shadow",
-                    })}
-                >
-                    Documentation
-                </Link>
-                <Link
-                    isExternal
-                    className={buttonStyles({ variant: "bordered", radius: "full" })}
-                    href={siteConfig.links.github}
-                >
-                    <GithubIcon size={20} />
-                    GitHub
-                </Link>
-            </div>
-
-            <div className="mt-8">
-                <Snippet hideCopyButton hideSymbol variant="bordered">
-                    <span>
-                        Get started by editing <Code color="primary">app/page.tsx</Code>
-                    </span>
-                </Snippet>
-            </div>
-        </section>
+        <div/>
     );
 }
